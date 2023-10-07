96253586ef191b1b0f64b2ba006b4b9d3f6c709e
rezmond <test@gmail.com>
2023-10-07 09:17:25 +0400

    Make the "CreateUserDto" conencted to "Prisma.UserCreateInput" type

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,15 +10,14 @@ import {
 
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(
-    @Body() createUserDto: { login: string; password: string; email: string },
-  ) {
+  create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 

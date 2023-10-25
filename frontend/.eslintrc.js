e954bfb3332676ffbf7504c9bfbe654361461d3f
rezmond <test@gmail.com>
2023-10-25 16:35:03 +0400

    Init the FSD eslint plugin

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  "extends": ["next/core-web-vitals", "../.eslintrc", "plugin:storybook/recommended"],
+  "extends": [
+    "next/core-web-vitals",
+    "../.eslintrc",
+    "plugin:storybook/recommended",
+    "@feature-sliced"
+  ],
   "parserOptions": {
     "tsconfigRootDir": __dirname
   },

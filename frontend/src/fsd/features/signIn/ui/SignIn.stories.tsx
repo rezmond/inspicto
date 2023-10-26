7e9b68889bf6ab11ed4b794f986554ce53bf0ad1
rezmond <test@gmail.com>
2023-10-26 18:17:17 +0400

    Add "SignIn" feature

---
+++ frontend/src/fsd/features/signIn/ui/SignIn.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { SignIn } from './SignIn';
+
+const meta = {
+  title: 'components/SignIn',
+  component: SignIn,
+  tags: ['autodocs'],
+} satisfies Meta<typeof SignIn>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const FirstState: Story = {
+  args: {
+    children: 'Sign in',
+  },
+};

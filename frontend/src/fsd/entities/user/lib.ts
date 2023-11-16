eef75afc959351268ee6836825366beb0a664395
rezmond <test@gmail.com>
2023-11-16 17:13:27 +0400

    Get rid of the "lib/utils.ts" file

--- frontend/src/fsd/entities/user/lib.ts
+++ frontend/src/fsd/entities/user/lib.ts
@@ -1,5 +1,6 @@
-import type { User } from '../model';
+import type { User } from './model';
 
+// TODO: rename the file
 export const getUserName = ({ firstName, lastName, login, email }: User) => {
   if (firstName || lastName) {
     return [firstName, lastName].join(' ');

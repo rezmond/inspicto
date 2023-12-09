360a99d87a8a4031510e9795e12c829a89aa2b62
rezmond <test@gmail.com>
2023-12-09 10:24:44 +0400

    Implement the sign in form request test

--- frontend/test/integration/features/session/SignInForm.test.tsx
+++ frontend/test/integration/features/session/SignInForm.test.tsx
@@ -1,3 +1,4 @@
+import { fireEvent } from '@testing-library/react';
 import { SignInForm } from '@/features/session';
 import { render } from '../../utils';
 
@@ -17,4 +18,26 @@ it('renders the SignInForm form accessible', () => {
   const { getByRole } = renderSignInForm();
 
   expect(getByRole('form')).toBeInTheDocument();
+});
+
+it('sends credentials to the server', () => {
+  const email = 'mail@test.ts';
+  const password = 'test-password';
+  const { appContext, getByRole, getByLabelText } = renderSignInForm();
+
+  const emailElement = getByRole('textbox', {
+    name: /email address/i,
+  });
+  const passwordElement = getByLabelText(/password/i);
+  const singInElement = getByRole('button', { name: /sign in/i });
+  fireEvent.change(emailElement, { target: { value: email } });
+  fireEvent.change(passwordElement, { target: { value: password } });
+  fireEvent.click(singInElement);
+
+  expect(appContext.api.session.signIn).toHaveBeenCalledTimes(1);
+  expect(appContext.api.session.signIn).toHaveBeenCalledWith({
+    email,
+    password,
+  });
+  expect(appContext.logger.error).not.toHaveBeenCalled();
 });
